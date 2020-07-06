fd98c6bf2979a1aab213cc848d75812b4a6d30d2
AndreyElin <musterspb@mail.ru>
2020-07-06 22:12:21 +0300

    Home work 7

---
+++ src/lesson7/server/Server.java
@@ -0,0 +1,75 @@
+package lesson7.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Vector;
+
+public class Server {
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+    public Server() {
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+
+        ServerSocket server = null;
+        Socket socket;
+
+        final int PORT = 8189;
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                System.out.println("socket.getRemoteSocketAddress(): " + socket.getRemoteSocketAddress());
+                System.out.println("socket.getLocalSocketAddress() " + socket.getLocalSocketAddress());
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+//    -----------------DZ TYT 1 FROM 2------------------------
+    void sendMsgToClient(ClientHandler from, String nickTo, String msg) {
+        for (ClientHandler o : clients) {
+            if (o.getNick().equals(nickTo)) {
+                o.sendMsg("от " + from.getNick() + ": " + msg);
+                from.sendMsg("клиенту " + nickTo + ": " + msg);
+                return;
+            }
+        }
+        from.sendMsg("Участника с ником " + nickTo + " нет в чат-комнате");
+    }
+//     ---------------------------------------------------------
+
+    void broadcastMsg(String msg) {
+        for (ClientHandler client : clients) {
+            client.sendMsg(msg);
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+}
